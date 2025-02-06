bf5eb5b2802da72dc19abfd7fd0e1e186e0671a6
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-02-06 12:32:01 -0600

    puerto y socket

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(4000,function(){
+    console.log('listening 4000');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('hice una conexión de socket',socket.id)
+});
