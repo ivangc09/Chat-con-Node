ff888f19ad1a0d59ea24878bd3957ed554366f8a
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-02-09 13:57:26 -0600

    cambiamos localhost por la url correcta

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://chat-con-node-production.up.railway.app/');
 
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
